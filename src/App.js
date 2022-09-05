a175db25149513e66a227558f42bc971ffe5fa3e
mohitrawat111 <109884777+mohitrawat111@users.noreply.github.com>
2022-09-05 17:29:00 +0530

    31-Fetching News category wise in NewsMonkey React App

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <NavBar />
-        <News pageSize={5} country="in" category="sports" />
-      </div>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<News key="general" pageSize={5} country="in" category="general" />} />
+            <Route exact path="/business" element={<News key="business" pageSize={5} country="in" category="business" />} />
+            <Route path="/entertainment" element={<News key="entertainment" pageSize={5} country="in" category="entertainment" />} />
+            <Route path="/health" element={<News key="health" pageSize={5} country="in" category="health" />} />
+            <Route path="/science" element={<News key="science" pageSize={5} country="in" category="science" />} />
+            <Route path="/sports" element={<News key="sports" pageSize={5} country="in" category="sports" />} />
+            <Route path="/technology" element={<News key="technology" pageSize={5} country="in" category="technology" />} />
+
+          </Routes>
+
+        </BrowserRouter>
+      </div >
     )
   }
 }
